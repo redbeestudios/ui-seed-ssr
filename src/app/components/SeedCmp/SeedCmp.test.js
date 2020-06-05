7e8bf04be7114769e8f93316feca71a4e0d2a010
Maxi <maxi.britez@redb.ee>
2020-06-05 14:32:59 -0300

    Add tests

---
+++ src/app/components/SeedCmp/SeedCmp.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import SeedCmp from './SeedCmp';
+
+describe('MyComponent', () => {
+  it('should render correctly', () => {
+    const message = 'aMessage';
+    const component = shallow(<SeedCmp greeting={message}/>);
+    expect(component).toMatchSnapshot();
+    expect(component.find('.seed__message').text()).toEqual(message);
+  });
+});
---
+++ src/app/components/Skeleton/Skeleton.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Skeleton from './index';
+
+describe('Skeleton', () => {
+  it('should render correctly', () => {
+    const component = shallow(<Skeleton/>);
+    expect(component).toMatchSnapshot();
+    expect(component.find('.skeleton__content')).toHaveLength(1);
+  });
+
+  it('should render a number of times', () => {
+    const quantity = 4;
+    const component = shallow(<Skeleton multiply={quantity}/>);
+    expect(component).toMatchSnapshot();
+    expect(component.find('.skeleton__content')).toHaveLength(quantity);
+  });
+});
